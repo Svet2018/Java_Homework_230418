1665eafd17fe36c103dc02da5151f4fd5939c885
Svet2018 <bulatova44@gmail.com>
2023-04-19 04:23:00 +0300

    Added tasks 0, 2

---
+++ src/hw_2/Task_1.java
@@ -0,0 +1,18 @@
+package hw_2;
+
+public class Task_1 {
+    public Task_1() {}
+
+    /* Task_1
+public class Task_1 {
+// Вам дается строка S и целочисленный массив индексов int index[s.length].
+// Напишите программу, которая перетасует символы в S таким образом,
+// что символ c i-й позиции переместится на индекс index[i] в результирующей строке.
+// Пример: s = “cba”, index = [3,2,1] result “abc”
+public static void main(String[] args) {
+}
+public String shuffle(final String s, final int[] index){
+return "";
+}
+     */
+}
